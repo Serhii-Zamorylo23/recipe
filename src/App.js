f3de68ba421a0b451a97ddce82fa503edf88d644
Serhii-Zamorylo23 <129538617+Serhii-Zamorylo23@users.noreply.github.com>
2025-03-08 15:01:32 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-
+import Recipe from 'Components/recipe/recipe.jsx';
+import React from 'react';
+import recipejson from './Components/recipe.json'
 function App() {
   return (
-    <></>
+    <Recipe recipes={recipejson} />
   );
 }
 
